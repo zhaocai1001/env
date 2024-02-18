0400c627d10f9ed3bb57fc152e86f1cfd8e09c34
GOgo8Go <153721248+GOgo8Go@users.noreply.github.com>
2024-02-18 18:43:43 +0800

    Update itvtest.py

--- itvtest.py
+++ itvtest.py
@@ -72,7 +72,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 10
+num_threads = 30
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True) 
     #t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
